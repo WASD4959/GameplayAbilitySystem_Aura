33ea4f38a798fe904bb0874e8542d4ee767ea734
putuitou <wasd_4959@163.com>
2026-04-05 13:50:54 +0800

    set up ability system component and attribute set

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright putuitou
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright putuitou
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	SetNetUpdateFrequency(100.f);
+}
